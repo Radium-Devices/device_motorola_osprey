d10ed48afd3e693071382561fb21a62db6cecc57
Akhil Narang <akhilnarang.1999@gmail.com>
2015-10-10 04:07:14 -0700

    osprey: Radium Bringup!
    * for beech @srisurya95
    * I'm assuming that kernel is at CM git, and blobs are merged in muppets
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- radium.mk
+++ radium.mk
@@ -14,13 +14,13 @@
 
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common radium stuff.
+$(call inherit-product, vendor/radium/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := cm_osprey
+PRODUCT_NAME := radium_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
@@ -28,3 +28,4 @@ PRODUCT_RELEASE_NAME := osprey
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Sri Harsha (srisurya95)"
